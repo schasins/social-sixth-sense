2e19905fbf7555b2edc1e48994cddfe406fabb37
schasins <schasins@kaopad.cs.berkeley.edu>
2015-04-20 22:24:41 -0700

    A first attempt at the hot and cold data visualization.  Uses a test dataset.

--- client/map.js
+++ client/map.js
@@ -9,18 +9,15 @@ function initialize() {
   }
   var div = document.getElementById('map-canvas');
   var $div = $(div);
-  $div.css("height", .5*$(window).height());
-  $div.css("width", .5*$(window).width());
-  $div.css("left", .25*$(window).width());
-  $div.css("top", .25*$(window).height());
+  $div.css("height", .8*$(window).height());
+  $div.css("width", .8*$(window).width());
+  $div.css("left", .1*$(window).width());
+  $div.css("top", .1*$(window).height());
   map = new google.maps.Map(div, mapOptions);
+  sliderSetup();
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-function displayDataPoints(dataPoints){
-  
-}
 
 var markers = [];
 
@@ -33,11 +30,11 @@ function displayDataPoints(dataPoints) {
 
   //put on the new
   for (var i = 0; i<dataPoints.length; i++){
-    var la = mostRecentPositions[i].lat;
-    var lo = mostRecentPositions[i].long;
-    var icon = "blue.png";
-    if (mostRecentPositions[i].approach > .5){
-	icon = "red.png";
+    var la = dataPoints[i].lat;
+    var lo = dataPoints[i].long;
+    var icon = "images/blue.png";
+    if (dataPoints[i].approach > .5){
+	icon = "images/red.png";
     }
     var myLatlng = new google.maps.LatLng(la,lo);
     var marker = new google.maps.Marker({
@@ -47,11 +44,12 @@ function displayDataPoints(dataPoints) {
     });
     markers.push(marker);
   }
+  //console.log("done");
 }
 
 function filterDataPoints(dataPoints, startTime, endTime) {
-	var fileteredDataPoints = [];
-	for (var i = 0; i++; i< dataPoints.length){
+	var filteredDataPoints = [];
+	for (var i = 0; i< dataPoints.length;i++){
 		if (startTime < dataPoints[i].time && endTime > dataPoints[i].time){
 			filteredDataPoints.push(dataPoints[i]);
 		}
@@ -60,28 +58,40 @@ function filterDataPoints(dataPoints, startTime, endTime) {
 }
 
 function update(dataPoints, startTime, endTime){
+        //console.log("update", dataPoints, startTime, endTime);
 	var filteredDataPoints = filterDataPoints(dataPoints, startTime, endTime);
+        //console.log(filteredDataPoints);
 	displayDataPoints(filteredDataPoints);
 }
 
 var timeMin;
 var timeMax;
-$(function() {
+function sliderSetup(){
+
+    var $slider = $(document.getElementById('slider-range'));
+    var $slider_wrapper = $(document.getElementById('slider-wrapper'));
+    $slider.css("width", .8*$(window).width());
+    $slider_wrapper.css("bottom", .1*$(window).height()-40);
+    $slider.css("left", .1*$(window).width());
+
     var times = _.pluck(currentPoints, "time");
-    var timeMin = Math.max.apply(Math, times);
-    var timeMax = Math.min.apply(Math, times);
+    timeMin = Math.min.apply(Math, times);
+    timeMax = Math.max.apply(Math, times);
+    console.log("timeMin: "+timeMin+" timeMax: "+timeMax);
     $( "#slider-range" ).slider({
       range: true,
       min: timeMin,
       max: timeMax,
-      values: [ 75, 300 ],
+      values: [ timeMin, timeMax ],
       slide: function( event, ui ) {
+	console.log(ui.values);
 	timeMin = ui.values[0];
 	timeMax = ui.values[1];
         update(currentPoints,timeMin,timeMax);
       }
     });
-  });
+    update(currentPoints,timeMin,timeMax);
+  }
 
-var testPoints = [{time: 5}];
+var testPoints = JSON.parse(testStr);
 var currentPoints = testPoints;
