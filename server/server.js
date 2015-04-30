a253ac94b25bc0d6c3a20ecf6ccc37d7ae79b6c6
schasins <schasins@gmail.com>
2015-04-30 15:58:11 -0700

    Server now sends back, rather than a generic ack, data about the closest other user, including his or her approachability and username.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var os = require('os');
 var path = require('path');
 var express = require('express');
 var socketio = require('socket.io');
@@ -32,16 +33,48 @@ express_app.post('/data', function(req, res) {
     console.log('got post with request body', req.body);
     var dict = req.body;
     if (dict.lat != "null"){
-        socketio_server.emit('gps', dict.gps);
-        fs.appendFile('server/gps.txt', (new Date().getTime())+','+dict.lat+','+dict.long+','+dict.approach+'\n', function(err) {
+        fs.appendFile('server/gps.txt', (dict.time+','+dict.lat+','+dict.long+','+dict.approach+','+dict.userid+'\n', function(err) {
             if (err) throw err;
             console.log('appended to gps.txt');
         });
 	//for testing purposes, also append to a file in client
-        fs.appendFile(p+'/data.csv', (new Date().getTime())+','+dict.lat+','+dict.long+','+dict.approach+'\n', function(err) {
+        fs.appendFile(p+'/data.csv', (dict.time+','+dict.lat+','+dict.long+','+dict.approach+','+dict.userid+'\n', function(err) {
             if (err) throw err;
             console.log('appended to data.csv');
         });  
+
+        //send back data that includes the id and aproachability of the nearest neighbor
+        var currTime = new Date().getTime());
+        var thresholdTime = currTime - 60000;
+        var bestDistancesq = 100000000000000000000;
+        var bestCells = null;
+        //note that altough writing to and processing a file is sufficient for our prototyping purposes,
+        //a more robust implementation should seriously use an actual database
+        fs.readFile('server/gps.txt', 'utf8', function (err,data) {
+          if (err) {
+            console.log(err);
+          }
+          var lines = data.split(os.EOL);
+          for (var i = lines.length; i >= 0; i--){
+            var line = lines[i];
+            var cells = line.split(",");
+            if (cells[0] < thresholdTime){
+                //only consider times within the last minute
+                break;
+            }
+            var distancesq = (dict.lat-cells[1])**2+(dict.long-cells[2])**2;
+            if (distancesq < bestDistancesq){
+                bestDistancesq = distancesq;
+                bestCells = cells;
+            }
+          }
+          if (bestCells !== null){
+            res.json({approach: bestCells[3], userid: bestCells[4]});
+          }
+          else {
+            res.json({approach: 0, userid: "defaultUser"}); //for testing purposes
+          }
+        });
     }
     res.end("yes");
 });
